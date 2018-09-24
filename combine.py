3e012c3ac705191557926b11ac225b31fa2cdabf
ywang297 <susiewang75@gmail.com>
2018-09-24 06:06:31 -0400

    combine data and remove categories having few reviews

--- combine.py
+++ combine.py
@@ -2,14 +2,30 @@
 """
 Created on Wed Sep 19 05:51:12 2018
 
-@author: Tao
+@author: Yishu
 """
 
 import pandas as pd
+import pickle 
+
 
 jimmy = pd.read_csv("jimmy-choo_reviews.csv")
 
-jimmy = jimmy[jimmy.index.values %2 !=0]
+bio = pd.read_csv("biotherm_reviews_290.csv")
+
+bbrown = pd.read_csv("bobbi-brown_reviews_1327.csv")
+
+ct = pd.read_csv("charlotte_tilbury_reviews_82.csv")
+
+evelom = pd.read_csv("eve-lom_reviews_378.csv")
+
+mac = pd.read_csv("mac-cosmetics_reviews_113.csv")
+
+muf = pd.read_csv("make-up-for-ever_reviews_2252.csv")
+
+sc = pd.read_csv("sephora-collection_reviews_5084.csv")
+
+ysl = pd.read_csv("yves-saint-laurent_reviews_1244.csv")
 
 kiehls = pd.read_csv("kiehls_reviews.csv")
 lancome = pd.read_csv("lancome_reviews.csv")
@@ -19,10 +35,52 @@ fresh = pd.read_csv("fresh_reviews.csv")
 clarins = pd.read_csv("clarins_reviews.csv")
 clinique = pd.read_csv("clinique_reviews.csv")
 
-sephora = pd.concat([kiehls, lancome, shiseido, dior, fresh, clarins, clinique])
+sephora_old = pd.concat([kiehls, lancome, shiseido, dior, fresh, clarins, clinique])
 
 
+shu = pickle.load(open("/Users/yishu/Documents/insight/shu_uemura.p", "rb"))
+philosophy = pickle.load(open("/Users/yishu/Documents/insight/philosophy.p", "rb"))
+
+
+
+sephora =pd.concat([sephora_old, shu, philosophy, bio, bbrown, ct, evelom, mac,
+                    muf, sc, ysl])
+
+sephora.loc[sephora['p_category']== 'Makeup Palettes', 'product']
+
+
+sephora['brand_name'].value_counts()
+
+sephora['p_category'].value_counts()
+
+skincare_list =['Skincare', 'Hidden Category']
+
+
+sephora.loc[sephora['p_category'].isin(skincare_list),'p_category'].value_counts().sum()
+
+sephora.loc[sephora['p_category'].isin(skincare_list), 'p_category'] = 'Skincare'
+
+
+makeup_list =['Makeup','Makeup Palettes']
+
+sephora.loc[sephora['p_category'].isin(makeup_list),'p_category'].value_counts().sum()
+
+sephora.loc[sephora['p_category'].isin(makeup_list), 'p_category'] = 'Makeup'
+
+
+remove_list = ['Value & Gift Sets', 'Makeup & Travel Cases', 'Men', 'Nail', 'Self Tanners',
+              'Tools & Brushes', 'Highlighter Brush', 'Foundation Brush', 
+              'Candles & Home Scents','Makeup Brushes & Applicators', 'Brush Sets & Accessories',
+             'Powder Brush', 'Crease Shadow Brush', 'Blush Brush','Mini Size']
+
+sephora.loc[sephora['p_category'].isin(remove_list),'p_category'].value_counts().sum()
+
+#24392-819
+sephora = sephora[~sephora['p_category'].isin(remove_list)]
 
 sephora.to_csv("sephora.csv", index=False, sep=',')
-import pickle
-pickle.dump(sephora, open("sephora.p", "wb"))
+
+sephora.to_pickle("sephora.p")
+
+
+
