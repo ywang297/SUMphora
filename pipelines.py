d3654331aed3ec45b378058eb710342420a71a89
ywang297 <susiewang75@gmail.com>
2018-10-12 13:41:24 -0400

    brand name needs to be changed in the pipeline for scraping

--- pipelines.py
+++ pipelines.py
@@ -20,7 +20,7 @@ class ValidateItemPipeline(object):
 class WriteItemPipeline(object):
 
     def __init__(self):
-        self.filename = 'clinique_reviews.csv'
+        self.filename = 'charlottetilbury_reviews.csv'
 
     def open_spider(self, spider):
         self.csvfile = open(self.filename, 'wb')
